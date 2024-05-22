ee47261e4faca84f0f13b19827e7137684fb67c7
Tabitha <spencersalmon.tabitha@gmail.com>
2024-05-22 10:45:57 -0400

    Add babel.config.js for Babel configuration

---
+++ babel.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+    presets: [
+        "@babel/preset-env", // Include if needed
+        "@babel/preset-react" // Include if needed
+    ],
+    plugins: [
+        "@babel/plugin-proposal-private-property-in-object"
+    ]
+};
