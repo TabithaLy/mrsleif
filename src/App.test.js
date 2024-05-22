027fe76960fd979abe6ced5d5fcf51aefc69b7b7
Tabitha <spencersalmon.tabitha@gmail.com>
2024-05-22 13:37:33 -0400

    Updated App.test.js to test for relevant text

--- src/App.test.js
+++ src/App.test.js
@@ -4,6 +4,6 @@ import App from './App';
 
 test('renders learn react link', async () => {
   render(<App />);
-  const linkElement = await screen.findByText(/learn react/i);
+  const linkElement = await screen.findByText(/For posts older than 2023, visit Mrs. Leif's Original Blog/i);
   expect(linkElement).toBeInTheDocument();
 });
