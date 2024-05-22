bef2a62b6b43a1409826f00a2255276558f2a415
Tabitha <spencersalmon.tabitha@gmail.com>
2024-05-22 11:26:11 -0400

    Fix warning by assigning act to window.ReactAct in index.js

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import ReactDOM from 'react-dom';
+import { act } from 'react-dom/test-utils'; // Import act from react-dom/test-utils
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
@@ -10,6 +11,9 @@ root.render(
     <App />
   </React.StrictMode>
 );
+
+// Assign act to window.ReactAct
+window.ReactAct = act;
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
